83a2b4809a1ab454c442abafc8eb0872d70498ac
Edwin Alaekpere <eddiesoft26@gmail.com>
2026-02-16 01:57:56 -0800

    health route

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./src/app";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, "0.0.0.0", () => { // Added 0.0.0.0 for Koyeb/Production
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  // Added 0.0.0.0 for Koyeb/Production
   console.log(`Server running on port ${PORT}`);
 });

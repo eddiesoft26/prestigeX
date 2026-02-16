76e0b82c5b58513e0c98ee7dc10a73c5ea077a8a
Edwin Alaekpere <eddiesoft26@gmail.com>
2026-02-16 02:13:17 -0800

    God abeg

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import dotenv from "dotenv";
 dotenv.config();
 import app from "./src/app";
+import prisma from "./src/prisma";
 
 const PORT = process.env.PORT || 8000;
 
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
+
+prisma
+  .$connect()
+  .then(() => console.log("DataBase connected successfuly"))
+  .catch((err) => {
+    console.error("database error:", err);
+    process.exit(1);
+  });
 
 app.listen(PORT, "0.0.0.0", () => {
   // Added 0.0.0.0 for Koyeb/Production
